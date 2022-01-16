fa6579f9d81a12887705be18466095910fcde69e
Tanha-Apu <80626228+Tanha-Apu@users.noreply.github.com>
2022-01-16 20:39:28 +0600

    Update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -17,7 +17,7 @@ def list_buttons(update, context):
         return sendMessage('Send a search key along with command', context.bot, update)
     buttons = button_build.ButtonMaker()
 
-    buttons.sbutton("Recursive", f"types {user_id} recu")
+    buttons.sbutton("Search", f"types {user_id} recu")
     buttons.sbutton("Cancel", f"types {user_id} cancel")
     button = InlineKeyboardMarkup(buttons.build_menu(2))
     sendMarkup('Choose option to list.', context.bot, update, button)
